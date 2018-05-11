c4abee93a8eff4c532f7a3e7939b832e42fe5043
jeanboy <jeanboy@foxmail.com>
2018-05-11 17:42:51 +0800

    optimize repository info

--- app/src/main/java/com/jeanboy/app/github/ui/activity/RepositoryInfoActivity.java
+++ app/src/main/java/com/jeanboy/app/github/ui/activity/RepositoryInfoActivity.java
@@ -6,16 +6,25 @@ import android.arch.lifecycle.Observer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
+import com.bumptech.glide.Glide;
 import com.jeanboy.app.github.R;
 import com.jeanboy.app.github.di.BaseDiActivity;
 import com.jeanboy.app.github.ui.vm.RepositoryInfoViewModel;
 import com.jeanboy.arch.base.ExtrasCallback;
 import com.jeanboy.arch.base.helper.ToolbarHelper;
+import com.jeanboy.arch.base.util.DateUtil;
 import com.jeanboy.arch.data.net.entity.RepositoryEntity;
+import com.jeanboy.arch.data.net.entity.UserInfoEntity;
+
+import java.util.Date;
 
 import javax.inject.Inject;
+
+import butterknife.BindView;
 
 public class RepositoryInfoActivity extends BaseDiActivity {
 
@@ -39,6 +48,19 @@ public class RepositoryInfoActivity extends BaseDiActivity {
     @Inject
     RepositoryInfoViewModel repositoryInfoViewModel;
 
+    @BindView(R.id.iv_avatar)
+    ImageView iv_avatar;
+    @BindView(R.id.tv_username)
+    TextView tv_username;
+    @BindView(R.id.tv_repos_name)
+    TextView tv_repos_name;
+    @BindView(R.id.tv_repos_desc)
+    TextView tv_repos_desc;
+    @BindView(R.id.tv_language)
+    TextView tv_language;
+    @BindView(R.id.tv_update_time)
+    TextView tv_update_time;
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_repository_info;
@@ -53,7 +75,7 @@ public class RepositoryInfoActivity extends BaseDiActivity {
 
     @Override
     protected void setupView(Bundle savedInstanceState) {
-        String title = getResources().getString(R.string.title_repos, username, repos);
+        String title = getResources().getString(R.string.title_repos);
         ToolbarHelper.setToolBarTitle(getToolbar(), title);
         ToolbarHelper.setToolbarHomeAsUp(this);
         ToolbarHelper.setStatusBarTranslucent(this);
@@ -73,5 +95,19 @@ public class RepositoryInfoActivity extends BaseDiActivity {
 
     private void refreshView(RepositoryEntity repositoryEntity) {
         Log.d(TAG, JSON.toJSONString(repositoryEntity));
+        UserInfoEntity owner = repositoryEntity.getOwner();
+        if(owner!=null){
+            String username = owner.getLogin();
+            tv_username.setText(username);
+            String avatarUrl = owner.getAvatar_url();
+            Glide.with(this).load(avatarUrl).into(iv_avatar);
+        }
+        tv_repos_name.setText(repositoryEntity.getFull_name());
+        tv_repos_desc.setText(repositoryEntity.getDescription());
+        tv_language.setText(repositoryEntity.getLanguage());
+        String updateAt = repositoryEntity.getUpdated_at();
+        Date date = DateUtil.formatUTC(updateAt);
+        String format = DateUtil.format(date.getTime(), DateUtil.FORMAT_DATE_24_FULL);
+        tv_update_time.setText(format);
     }
 }
